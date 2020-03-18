ed0d005e9778a49598b198117107f51644f272e9
Lauren Birts <birtslauren@gmail.com>
2020-03-18 04:21:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,9 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key]
     value = hash[key] += 1
-    return value
+    return hash
     else
       value = hash[key] = 1
-      return value
+      return hash
   end
 end
